30fe8da007e7d2f4659e5c3136a7ce9bdce9e8d5
neoan <stefan.roehrl@gmail.com>
2020-01-24 16:18:56 -0500

    Update route.php

--- templates/route.php
+++ templates/route.php
@@ -4,12 +4,25 @@ namespace Neoan3\Components;
 
 use Neoan3\Frame\Vastn3;
 
+/**
+ * Class {{name}}
+ * @package Neoan3\Components
+ */
 class {{name}} extends Vastn3
 {
+    /**
+     * @var array
+     */
+    private $vueComponents = [];
+    
+    /**
+     * init route 
+     */
     function init()
     {
         $this
             ->hook('main', '{{name.camel}}', [])
+            ->vueComponents($this->vueComponents, [])
             ->output();
     }
 }
