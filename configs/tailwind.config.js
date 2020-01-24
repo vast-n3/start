7a180cb5e13f532c8ecf621dccd9a6cc8c281128
neoan <stefan.roehrl@gmail.com>
2020-01-24 13:31:55 -0500

    Create tailwind.config.js

---
+++ configs/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        success: "var(--color-success)",
+        warning: "var(--color-success)",
+        danger: "var(--color-success)",
+      },
+    }
+  },
+  variants: {},
+  plugins: [
+      
+  ]
+};
